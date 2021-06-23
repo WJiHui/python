572ddd727a0fbff681353e164ac9a132be9d42e0
weijihui <weijihuiall@163.com>
2021-06-23 16:07:44 +0800

    add tuple.py

---
+++ data_structure/tuple.py
@@ -0,0 +1,12 @@
+def define_tuple():
+    a = (1)  # 这里的（）只代表运算顺序
+    print(type(a))  # <class 'int'>
+
+    b = (1,)
+    print(type(b))  # <class 'tuple'>
+
+    # 结论如果申明元组的时候只有一个元素，结尾要加 ，
+
+
+if __name__ == '__main__':
+    define_tuple()
