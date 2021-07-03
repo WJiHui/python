c91662b0c4e1227cb426610c49053ae90c8f4d5a
weijihui <weijihuiall@163.com>
2021-07-03 10:11:48 +0800

    使用zip生成元组

--- data_structure/tuple.py
+++ data_structure/tuple.py
@@ -1,3 +1,4 @@
+
 def define_tuple():
     a = (1)  # 这里的（）只代表运算顺序
     print(type(a))  # <class 'int'>
@@ -9,5 +10,23 @@ def define_tuple():
     #    如果声明元组的时候只有一个元素，结尾要加","
 
 
+def init_dict_zip():
+    print("\n", "使用zip返回元组")
+    a = [
+        'a,1',
+        'a,2',
+        'a,3'
+    ]
+    b = [
+        'b,1',
+        'b,2',
+        'b,3',
+        'b,4'
+    ]
+    for i in zip(a, b):  # 返回一个iterable zip object
+        print(i)
+
+
 if __name__ == '__main__':
     define_tuple()
+    init_dict_zip()
