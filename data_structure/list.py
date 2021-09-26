5f45f7bca1c33050d86de5d06dc9d980b27cdc28
weijihui <weijihuiall@163.com>
2021-09-26 14:30:03 +0800

    update list.py

--- data_structure/list.py
+++ data_structure/list.py
@@ -1,14 +1,17 @@
 def reverse(sequence):
     """
     sequence can be list or tuple
+    list return <class 'list_reverseiterator'>
+    tuple return <class 'reversed'>
     """
-    # list return <class 'list_reverseiterator'>
-    # tuple return <class 'reversed'>
+
+    print(reversed(sequence))
     print(type(reversed(sequence)))
 
-    #for i in reversed(sequence):
-    #    print(i)
+    for i in reversed(sequence):
+        print(i)
+
 
 if __name__ == '__main__':
-    reverse([1,2,3,4])
-    reverse(('a','b','v','d'))
+    reverse([1, 2, 3, 4])
+    reverse(('a', 'b', 'v', 'd'))
